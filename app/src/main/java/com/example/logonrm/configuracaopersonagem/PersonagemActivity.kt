12e72d9944b809d84eef295520c9e78564aa01f5
logonrm <logonrm@fiap.com.br>
2018-03-09 22:44:19 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/configuracaopersonagem/PersonagemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.logonrm.configuracaopersonagem
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PersonagemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personagem)
+    }
+}
